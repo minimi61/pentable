0ff912ee138a87b1462c1554459ac92044a59e66
minimi61 <new1573@naver.com>
2022-06-27 20:47:34 +0900

    add style commit

--- about.js
+++ about.js
@@ -1,14 +1,35 @@
 const aboutClick = document.querySelector(".about");
-const img = document.querySelector("img");
-// const interiorImg = document.querySelector("interior-img");
-// console.log(img);
+const imgInDiv =  document.querySelector(".img-in-div");
+const interiorImg = document.querySelector(".interior-img");
 
 const aboutPageOpen = () => {
-    const aboutPageApply = aboutClick.classList.toggle("aboutPage");
-    // const imgRemove = aboutClick.classList.remove("interior-img");
-    // console.log(imgRemove)
+    const containsAbout = aboutClick.classList.contains('aboutPageBtn');
+    if (!containsAbout) {
+        aboutClick.classList.add('aboutPageBtn');
+        aboutClick.style.animation = 'fadeOut 0.5s';
+        aboutClick.style.webkitAnimation = 'fadeOut 0.5s';
+    setTimeout(() => {
+        aboutClick.style.animation = 'fadeIn 0.5s';
+        aboutClick.style.webkitAnimation = 'fadeIn 0.5s';
+        interiorImg.style.display = 'none'
+        imgInDiv.classList.add('aboutPage')
+    }, 500);
+}
+else {
+   
+    imgInDiv.style.animation = 'fadeOut 0.5s';
+    imgInDiv.style.webkitAnimation = 'fadeOut 0.5s';
+    
+    setTimeout(() => {
+        aboutClick.classList.remove('aboutPageBtn');
+        aboutClick.style.animation = 'fadeIn 0.5s';
+        aboutClick.style.webkitAnimation = 'fadeIn 0.5s';
+        imgInDiv.classList.remove('aboutPage');
+        interiorImg.style.display = 'block'
+    }, 500);
+}
+
 }
 
 aboutClick.addEventListener("click", aboutPageOpen)
-
 
