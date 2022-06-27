4db6873ca5b62a8f4eb5fab471c99cf71270e951
minimi61 <new1573@naver.com>
2022-06-27 21:47:18 +0900

    softly animation commit

--- about.js
+++ about.js
@@ -4,32 +4,34 @@ const interiorImg = document.querySelector(".interior-img");
 
 const aboutPageOpen = () => {
     const containsAbout = aboutClick.classList.contains('aboutPageBtn');
+    
     if (!containsAbout) {
         aboutClick.classList.add('aboutPageBtn');
-        aboutClick.style.animation = 'fadeOut 0.5s';
-        aboutClick.style.webkitAnimation = 'fadeOut 0.5s';
-    setTimeout(() => {
-        aboutClick.style.animation = 'fadeIn 0.5s';
-        aboutClick.style.webkitAnimation = 'fadeIn 0.5s';
-        interiorImg.style.display = 'none'
-        imgInDiv.classList.add('aboutPage')
-    }, 500);
-}
-else {
-   
-    imgInDiv.style.animation = 'fadeOut 0.5s';
-    imgInDiv.style.webkitAnimation = 'fadeOut 0.5s';
+
+            imgInDiv.style.animation = 'fadeOut 0.6s';
+            imgInDiv.style.webkitAnimation = 'fadeOut 0.6s';
+            setTimeout(() => {
+                interiorImg.style.display = 'none'
+                imgInDiv.style.animation = 'fadeIn 0.5s';
+                imgInDiv.style.webkitAnimation = 'fadeIn 0.5s';
+                imgInDiv.classList.add('aboutPage')
+            }, 500);
+ 
+    }
+        
+    else {
+        imgInDiv.style.animation = 'fadeOut 0.6s';
+        imgInDiv.style.webkitAnimation = 'fadeOut 0.6s';
     
-    setTimeout(() => {
-        aboutClick.classList.remove('aboutPageBtn');
-        aboutClick.style.animation = 'fadeIn 0.5s';
-        aboutClick.style.webkitAnimation = 'fadeIn 0.5s';
-        imgInDiv.classList.remove('aboutPage');
-        interiorImg.style.display = 'block'
-    }, 500);
-}
+        setTimeout(() => {
+            aboutClick.classList.remove('aboutPageBtn');
+            imgInDiv.style.animation = 'fadeIn 0.5s';
+            imgInDiv.style.webkitAnimation = 'fadeIn 0.5s';
+            imgInDiv.classList.remove('aboutPage');
+            interiorImg.style.display = 'block'
+        }, 500);
 
+    }
 }
-
 aboutClick.addEventListener("click", aboutPageOpen)
 
